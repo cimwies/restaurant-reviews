d5c0a6a968884bfd7776fa189494a4a2ca614682
Claudia <hello@allegro-online.net>
2018-06-30 22:12:29 +0200

    Set DATABASE_URL() for GitHub

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,10 +7,18 @@ class DBHelper {
    * Database URL.
    * Change this to restaurants.json file location on your server.
    */
+
+  /*  local settings
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
     return `http://localhost:${port}/data/restaurants.json`;
   }
+   */
+
+static get DATABASE_URL() {
+  const port = 80
+  return `https://cimwies.github.io/restaurant-reviews/data/restaurants.json`;
+}
 
   /**
    * Fetch all restaurants.
