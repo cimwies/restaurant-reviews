25c1e870abb4cc687a91faa0ac3298c01e563862
Claudia <hello@allegro-online.net>
2018-06-30 23:45:51 +0200

    Scope

--- js/main.js
+++ js/main.js
@@ -217,7 +217,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
       navigator.serviceWorker
-      .register('/sw.js')
+      .register('/sw.js', {scope: '/'})
       .then((reg) => {
         // Registration was successful :)
         if(reg.installing) {
