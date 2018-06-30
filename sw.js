e27c33d4d0d4b018435b737dd2f0609f892578e6
Claudia <hello@allegro-online.net>
2018-07-01 00:32:49 +0200

    SW

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ self.addEventListener('install', e => {
     caches.open('staticCacheName')
     .then( cache =>  {
       return cache.addAll([
-        '/restaurant-reviews',
+        '/',
         '/index.html',
         '/restaurant.html',
         '/css/styles.css',
