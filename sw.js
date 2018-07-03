b398dc2816ca2a5b30b9c3e3d9cc3f14587da22f
Claudia <hello@allegro-online.net>
2018-07-03 22:11:01 +0200

    Add photo cache to service worker

--- sw.js
+++ sw.js
@@ -1,32 +1,45 @@
-/**
- * Servce worker script.
- */
-let staticCacheName = 'restaurant-static-v1';
 
+const staticCacheName = 'restaurant-static-v1';
+const imagesCacheName = 'restaurant-images-v1';
+
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/js/dbhelper.js',
+  '/data/restaurants.json'
+]
+
+/** 
+ * initializing a cache, and cache the App Shell 
+ * and static assets using the Cache API 
+ */
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('staticCacheName')
-    .then( cache =>  {
-      return cache.addAll([
-        '/restaurant-reviews',
-        '/index.html',
-        '/restaurant.html',
-        '/css/styles.css',
-        '/js/main.js',
-        '/js/restaurant_info.js',
-        '/js/dbhelper.js',
-        '/data/restaurants.json'
-        //TODO die Bilder nicht vergessen?
-      ])
-      .catch( err => {
-        console.log("Caches open failed: " + err);
-      });
+    .then(cache => cache.addAll(filesToCache))
+    .catch( err => {
+      console.log("Caches open failed: " + err);
     })
-  );
+  )
 });
 
+/**
+ * The activation stage is the third step, 
+ * once the service worker has been successfully 
+ * registered and installed.
+ * At this point, the service worker will be able 
+ * to work with new page loads. 
 
-/* Cache first strategy and cleaning up old caches */
+/* Cache first strategy and cleaning up old caches 
+ * update a Service Worker, and when the register code 
+ * is run, it will be updated also cleanup old caches and things 
+ * associated with the old version but unused in the new version 
+ * of the service worker *
+ */
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
@@ -43,17 +56,30 @@ self.addEventListener('activate', e => {
   );
 });
 
-/* What resouces to fetch and fallback solution*/
+/**
+ * A fetch event is fired when a resource is requested on the network. 
+ */
 self.addEventListener('fetch', e => {
-  let requestUrl = new URL(e.request.url);
+  const requestUrl = new URL(e.request.url);
+
   if (requestUrl.origin === location.origin) {
-    if (requestUrl.pathname === '/') {
-      // if path is root stay in root - not necessary?
+    if (requestUrl.pathname === 'index.html') {
+      // required if files are in a different directory
       e.respondWith(caches.match('/'));
       return;
     }
   }
-  e.respondWith(
+  if ((requestUrl.pathname.startsWith('/img/header/')) || (requestUrl.pathname.startsWith('/img/thumbs/'))) {
+      e.respondWith(servePhoto(e.request));
+      return;
+  }
+
+/** 
+ * use the Cache API to check if the request URL was already stored
+ * in the cached responses, and return the cached response if this is the case.
+ * Otherwise, it executes the fetch request and returns it 
+ */  
+    e.respondWith(
     caches.match(e.request)
     .then( response => {
       return response || fetch(e.request);
@@ -61,7 +87,21 @@ self.addEventListener('fetch', e => {
   );
 });
 
-/* Speed up install and activate */
+function servePhoto(request) {
+  return caches.open(imagesCacheName)
+  .then(cache => {
+      return cache.match(request)
+      .then(response => (
+          response || cacheAndFetch(cache, request)
+      ));
+  });
+}
+
+function cacheAndFetch(cache, request) {
+    cache.add(request);
+    return fetch(request);
+}
+
 self.addEventListener('message', e => {
   if (e.data.action === 'skipWaiting') {
     self.skipWaiting();
